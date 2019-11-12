be6ab2e2f53959c0d2ff59baead53b77978abcbd
jmanchuck <jjcheung0000@gmail.com>
2019-11-12 07:49:36 +0000

    Animation added

--- solver.py
+++ solver.py
@@ -3,7 +3,6 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from time import process_time
-import pandas as pd
 
 c = 2.99 * (10 ** 8)
 mu = 1.26 * (10 ** (-6))
@@ -36,29 +35,9 @@ pulseMid = 3 * sigma_t
 
 time = 0
 size = int(l_x / ds)
-print(size)
 h = np.zeros((size, size))
 ex = np.zeros((size + 1, size))
 ey = np.zeros((size, size + 1))
-
-def test_gaussian():
-
-    data = []
-
-    time = 0
-
-    while time < dt * 500:
-        gauss = (1 / (sigma_t * math.sqrt(2 * math.pi))) * (math.exp(-((time - pulseMid) ** 2) / (2 * (sigma_t ** 2))))
-        data.append(gauss)
-        time += dt
-        print(gauss)
-
-    plt.plot(data)
-    plt.show()
-
-# test_gaussian()
-#
-# exit()
 
 fig, ax = plt.subplots()
 
@@ -113,50 +92,3 @@ anim = animation.FuncAnimation(fig, func=animate, frames=np.arange(0, end_time, 
 
 plt.show()
 exit()
-
-# while time < end_time:
-#
-#     h_prev = h
-#     ex_prev = ex
-#     ey_prev = ey
-#
-#     # update h
-#     h = h_prev + constant_mu * ((ex_prev[1:] - ex_prev[:-1]) - (ey_prev[:, 1:] - ey_prev[:, :-1]))
-#
-#     # override h bottom left with Gaussian
-#     if 0 < time < 350 * dt:
-#         h[50][50] = (-time + pulseMid) * (1 / (sigma_t * math.sqrt(2 * math.pi))) * (math.exp(-((time - pulseMid) ** 2) / (2 * (sigma_t ** 2))))
-#
-#     # h[0], h[-1], h[:, 0], h[:, -1] = 0, 0, 0, 0
-#
-#     ex[1:-1, :] = ex_prev[1:-1, :] + constant_e * (h[1:, :] - h[:-1, :])
-#     ey[:, 1:-1] = ey_prev[:, 1:-1] - constant_e * (h[:, 1:] - h[:, :-1])
-#
-#     # ex[0] = ex_prev[0] + (dt / (ds * epsilon)) * (h[0])
-#     # ex[-1] = ex_prev[-1] + (dt / (ds * epsilon)) * (-h[-1])
-#     #
-#     # ey[:, 0] = ey_prev[:, 0] + (dt / (ds * epsilon)) * (h[:, 0])
-#     # ey[:, -1] = ey_prev[:, -1] + (dt / (ds * epsilon)) * (-h[:, -1])
-#
-#     # ex[0],  ex[-1], ex[:, -1], ex[:, 0] = 0, 0, 0, 0
-#     #
-#     # ey[0], ey[-1], ey[:, 0], ey[:, -1] = 0, 0, 0, 0
-#
-#     time += dt
-#     step += 1
-#     # print(step)
-#
-#     # dfx = pd.DataFrame(ex)
-#     # print(dfx)
-#     # # df = pd.DataFrame(h)
-#     # print(df)
-#
-#     mesh = ax.pcolormesh(h)
-#     start = process_time()
-#     fig.savefig('figs/' + str(step) + '.png')
-#     end = process_time()
-#     print(end - start)
-
-
-
-
